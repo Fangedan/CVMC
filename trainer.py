0f24f6299d9b789813dff920f23e297dd37e266b
Andrew Lin <89644316+Fangedan@users.noreply.github.com>
2025-03-29 20:59:16 -0500

    Modified trainer.py to save only the best model.

--- trainer.py
+++ trainer.py
@@ -45,11 +45,11 @@ def parser():
 
     args.add_argument("--lr", type=float, default=0.0001, help="Learning Rate")
     args.add_argument('--lrDecay', type=float, default=0.95, help='Learning rate decay rate')
-    args.add_argument('--maxEpoch', type=int, default=10, help='Maximum number of epochs')
+    args.add_argument('--maxEpoch', type=int, default=25, help='Maximum number of epochs')
     args.add_argument('--testInterval', type=int, default=1, help='Test and save every [testInterval] epochs')
-    args.add_argument('--batchSize', type=int, default=128, help='Dynamic batch size, default is 500 frames.')
+    args.add_argument('--batchSize', type=int, default=32, help='Dynamic batch size, default is 500 frames.')
     args.add_argument('--nDataLoaderThread', type=int, default=4, help='Number of loader threads')
-    args.add_argument('--datasetPath', type=str, default="/notebooks/AVDIAR_ASD/", help='Path to the ASD Dataset')
+    args.add_argument('--datasetPath', type=str, default="/mnt/data/datasets/AVDIAR_ASD/", help='Path to the ASD Dataset')
     args.add_argument('--loadAudioSeconds', type=float, default=3, help='Number of seconds of audio to load for each training sample')
     args.add_argument('--loadNumImages', type=int, default=1, help='Number of images to load for each training sample')
     args.add_argument('--savePath', type=str, default="exps/exp1")
@@ -104,13 +104,16 @@ def main(args):
 
     mAPs = []
     scoreFile = open(args.scoreSavePath, "a+")
-
+    bestmAP = 0
     while(1):        
         loss, lr = s.train_network(epoch = epoch, loader = trainLoader, **vars(args))
         
         if epoch % args.testInterval == 0:        
-            s.saveParameters(args.modelSavePath + "/model_%04d.model"%epoch)
+            
             mAPs.append(s.evaluate_network(epoch = epoch, loader = valLoader, **vars(args)))
+            if mAPs[-1] > bestmAP:
+                bestmAP = mAPs[-1]
+                s.saveParameters(args.modelSavePath + "/best.model")
             print(time.strftime("%Y-%m-%d %H:%M:%S"), "%d epoch, mAP %2.2f%%, bestmAP %2.2f%%"%(epoch, mAPs[-1], max(mAPs)))
             scoreFile.write("%d epoch, LR %f, LOSS %f, mAP %2.2f%%, bestmAP %2.2f%%\n"%(epoch, lr, loss, mAPs[-1], max(mAPs)))
             scoreFile.flush()
